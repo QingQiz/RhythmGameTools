4a9acd490df75898436c124e5bfd411da6a2ee55
QingQiz <sofeeys@outlook.com>
2023-10-24 21:21:34 +0800

    FEAT: bms filename fixer: shift-jis -> gb2312

---
+++ BmsFilenameFIxer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace BmsFilenameFixer;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //var toFix = @"F:\workspace\bms\packs\BOFXV\1. BOFXV - THE BMS OF FIGHTERS eXtreme Violence - （No.1~No.149）";
+        var toFix = @"F:\workspace\bms\packs\G2R2018";
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        var jis = Encoding.GetEncoding("Shift-JIS");
+        var enc = Encoding.GetEncoding("GB2312");
+
+        var dirs = Directory.GetDirectories(toFix, "*", SearchOption.AllDirectories);
+
+        foreach (var dir in dirs.Reverse())
+        {
+            var parent  = Path.GetDirectoryName(dir);
+            var oldName = Path.GetFileName(dir);
+            var newName = jis.GetString(enc.GetBytes(oldName));
+
+            if (oldName == newName) continue;
+
+            var newPath = Path.Combine(parent, newName);
+
+            Console.WriteLine($"{oldName} -> {newName}");
+            Directory.Move(dir, newPath);
+        }   
+
+        var files = Directory.GetFiles(toFix, "*", SearchOption.AllDirectories);
+        foreach (var file in files)
+        {
+            var parent  = Path.GetDirectoryName(file);
+            var oldName = Path.GetFileName(file);
+            var newName = jis.GetString(enc.GetBytes(oldName));
+
+            if (oldName == newName) continue;
+
+            var newPath = Path.Combine(parent, newName);
+
+            Console.WriteLine($"{oldName} -> {newName}");
+            File.Move(file, newPath);
+        }
+    }
+}
