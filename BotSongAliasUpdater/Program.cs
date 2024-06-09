7fbc87ee258481c4d6954a3a56742affeea8021d
QingQiz <sofeeys@outlook.com>
2024-06-10 03:11:44 +0800

    FEAT: 歌曲别名更新器添加了默认的繁简转换后的别名，并移除重复/被包含的别名

--- BotSongAliasUpdater/Program.cs
+++ BotSongAliasUpdater/Program.cs
@@ -1,8 +1,33 @@
-﻿using System.Text;
+﻿using System.Dynamic;
+using System.Text;
+using System.Text.Json;
+using Flurl.Http;
+
+namespace BotSongAliasUpdater;
 
 public static class Program
 {
-    public static void Merge(string tsv, string txt)
+    private static HashSet<string> GetDefault(string key)
+    {
+        var simplified = ConvertToSimplified(key);
+
+        return simplified.Equals(key, StringComparison.OrdinalIgnoreCase)
+            ? new HashSet<string>()
+            : new HashSet<string> { ConvertToSimplified(key) };
+    }
+    
+    private static string Escape(string s)
+    {
+        return s.Contains('"') ? $"\"{s.Replace("\"", "\"\"")}\"" : s;
+    }
+
+    private static string Trim(string s)
+    {
+        if (s[0] == '"' && s[^1] == '"') return s[1..^1];
+        return s;
+    }
+
+    private static void Merge(HashSet<string> titles, string tsv, string txt)
     {
         var lines = File.ReadAllLines(tsv);
         var dict  = new Dictionary<string, HashSet<string>>();
@@ -12,12 +37,11 @@ public static class Program
             var split = line.Split('\t');
             var key   = split[0];
 
-            if (!dict.ContainsKey(key)) dict.Add(key, new HashSet<string>());
-
             foreach (var i in split[1..]
-                         .Select(x => x.Trim('"').Replace("\"\"", "\""))
+                         .Select(x => Trim(x).Replace("\"\"", "\""))
                          .Where(x => !string.IsNullOrWhiteSpace(x)))
             {
+                if (!dict.ContainsKey(key)) dict[key] = GetDefault(key);
                 dict[key].Add(i);
             }
         }
@@ -27,35 +51,108 @@ public static class Program
         foreach (var i in txtLines)
         {
             var split = i.Split('\t');
-            var key   = split[0];
+            var key   = Escape(split[0]);
             var value = split[1];
 
-            if (!dict.ContainsKey(key)) dict.Add(key, new HashSet<string>());
+            if (!dict.ContainsKey(key)) dict[key] = GetDefault(key);
+            dict[key].Add(value);
+        }
 
-            dict[key].Add(value.Contains('"') ? $"\"{value.Replace("\"", "\"\"")}\"" : value);
+        foreach (var t in titles.Select(Escape).Where(t => !dict.ContainsKey(t)))
+        {
+            dict[t] = GetDefault(t);
+        }
+
+        foreach (var key in dict.Keys)
+        {
+            var set      = dict[key].ToList();
+            var toRemove = new HashSet<string>();
+
+            for (var i = 0; i < set.Count; i++)
+            {
+                var toCheck = set[i];
+
+                if (toCheck.Equals(key, StringComparison.OrdinalIgnoreCase))
+                {
+                    toRemove.Add(toCheck);
+                    continue;
+                }
+
+                for (var j = 0; j < i; j++)
+                {
+                    var cmp = set[j];
+
+                    if (toRemove.Contains(cmp)) continue;
+                    if (!cmp.Contains(toCheck, StringComparison.OrdinalIgnoreCase)) continue;
+
+                    toRemove.Add(toCheck);
+                    break;
+                }
+            }
+
+            foreach (var i in toRemove)
+            {
+                dict[key].Remove(i);
+            }
         }
 
         var sb = new StringBuilder();
 
         foreach (var (key, value) in dict)
         {
+            if (!value.Any()) continue;
+
             sb.Append(key);
             sb.Append('\t');
-            sb.AppendJoin('\t', value);
+            sb.AppendJoin('\t', value.Select(v => v.Contains('"') ? $"\"{v.Replace("\"", "\"\"")}\"" : v));
             sb.Append('\n');
         }
 
         File.WriteAllText(tsv, sb.ToString());
-
-        File.Copy(txt, txt + ".bak", true);
-        File.WriteAllText(txt, "");
     }
 
-    public static void Main(string[] args)
+    private static Dictionary<char, char>? _mapJ2T;
+
+    private static Dictionary<char, char> MapJ2T => _mapJ2T ??= File.ReadAllLines("dict.txt")
+        .Select(x => x.Split('\t'))
+        .GroupBy(x => x[1])
+        .Where(x => x.Count() == 1)
+        .ToDictionary(x => x.Key[0], x => x.First()[0][0]);
+
+    private static Dictionary<char, char>? _mapT2S;
+
+    private static Dictionary<char, char> MapT2S => _mapT2S ??= File.ReadAllLines("dict2.txt")
+        .Select(x => x.Split('\t'))
+        .GroupBy(x => x[0][0])
+        .Where(x => x.Count() == 1)
+        .ToDictionary(x => x.Key, x => x.First()[1][0]);
+
+    static string ConvertToSimplified(string traditionalChinese)
     {
-        Merge(@"C:\Users\sofee\RiderProjects\QQBOT\Marisa.Frontend\public\assets\maimai\aliases.tsv",
-            @"C:\Users\sofee\Desktop\temp\maimai\MaiMaiSongAliasTemp.txt");
-        Merge(@"C:\Users\sofee\RiderProjects\QQBOT\Marisa.Plugin.Shared\Resource\Chunithm\aliases.tsv",
-            @"C:\Users\sofee\Desktop\temp\chunithm\ChunithmSongAliasTemp.txt");
+        var sbT = new StringBuilder();
+        var sbS = new StringBuilder();
+
+        foreach (var c in traditionalChinese) sbT.Append(MapJ2T.ContainsKey(c) ? MapJ2T[c] : c);
+        foreach (var c in sbT.ToString()) sbS.Append(MapT2S.ContainsKey(c) ? MapT2S[c] : c);
+
+        return sbS.ToString();
+    }
+
+    public static async Task Main(string[] args)
+    {
+        var chuMd = (JsonSerializer.Deserialize<ExpandoObject[]>(
+                await File.ReadAllTextAsync("C:/Users/sofee/Desktop/workspace/QQBOT/Marisa.Frontend/public/assets/chunithm/SongInfo.json")
+            )! as dynamic[])
+            .Select(x => x.Title.ToString()).Where(x => x != null).Cast<string>().ToHashSet();
+
+        var maiMd = (await "https://www.diving-fish.com/api/maimaidxprober/music_data"
+                .GetJsonAsync<ExpandoObject[]>() as dynamic[])
+            .Select(x => x.title.ToString()).Where(x => x != null).Cast<string>().ToHashSet();
+
+        Merge(maiMd, @"C:\Users\sofee\Desktop\workspace\QQBOT\Marisa.Frontend\public\assets\maimai\aliases.tsv",
+            @"f:\MarisaBotTemp\maimai\MaiMaiSongAliasTemp.txt");
+
+        Merge(chuMd, @"C:\Users\sofee\Desktop\workspace\QQBOT\Marisa.Frontend\public\assets\chunithm\aliases.tsv",
+            @"f:\MarisaBotTemp\chunithm\ChunithmSongAliasTemp.txt");
     }
 }
