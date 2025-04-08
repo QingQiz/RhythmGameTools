5084edb7ac496c572c78236d010884cb7b26c9da
QINGQIZ <sofeeys@outlook.com>
2025-04-08 19:51:28 +0800

    ref: 更新了代码结构，把api移到了osuapi里

--- OsuBeatmapDownloader/OsuApi.cs
+++ OsuBeatmapDownloader/OsuApi.cs
@@ -1,13 +1,20 @@
-﻿using System.Net;
+﻿using System.Globalization;
+using System.Net;
 using System.Reflection;
 using System.Web;
+using Flurl;
 using Flurl.Http;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace OsuBeatmapDownloader;
 
+public record BeatmapSet(int Id, string Title, string Artist, string Creator);
+
 public static class OsuApi
 {
+    #region req with auth
+
     private static string? _token;
     private static DateTime? _tokenExpire;
 
@@ -32,9 +39,9 @@ public static class OsuApi
     private const string FakeUserAgent =
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36";
 
-    public const string ApiRoot = "https://osu.ppy.sh/api/v2";
+    private const string ApiRoot = "https://osu.ppy.sh/api/v2";
 
-    public static readonly IConfigurationRoot Config =
+    private static readonly IConfigurationRoot Config =
         new ConfigurationBuilder().AddUserSecrets(Assembly.GetExecutingAssembly()).Build();
 
     /// <summary>
@@ -59,11 +66,13 @@ public static class OsuApi
         _tokenExpire = DateTime.Now + TimeSpan.FromSeconds(res.expires_in);
     }
 
-    public static IFlurlRequest Request(string uri)
+    private static IFlurlRequest Request(string uri)
     {
         return uri.WithHeader("Accept", "application/json")
             .WithOAuthBearerToken(Token);
     }
+
+    #endregion
 
     #region Beatmap Downloader
 
@@ -130,6 +139,126 @@ public static class OsuApi
         }
 
         return await DownloadBeatmapInner();
+    }
+
+    #endregion
+
+    #region high level api
+
+    public static async Task<List<BeatmapSet>> GetRankedBeatmapSets()
+    {
+        var list = new List<BeatmapSet>();
+        var cache =
+            from file in Directory.GetFiles(".", "*-ranked.json")
+            select JsonConvert.DeserializeObject<List<BeatmapSet>>(File.ReadAllTextAsync(file).Result);
+        foreach (var i in cache) list.AddRange(i);
+        list = list.DistinctBy(x => x.Id).ToList();
+
+        while (true)
+        {
+            var date = (
+                from file in Directory.GetFiles(".", "*-ranked.json")
+                select Path.GetFileNameWithoutExtension(file)
+                into dateInFile
+                select dateInFile.Split('-')[0]
+                into dateInFile
+                select DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(dateInFile))
+                into last
+                select last - TimeSpan.FromHours(2)
+            ).Aggregate(DateTimeOffset.MinValue, (current, last) => last > current ? last : current);
+
+            const int limit = 500;
+            var url = "https://osu.ppy.sh/api/get_beatmaps".SetQueryParams(new
+            {
+                k = OsuApi.Config["apiKey"],
+                // utc time in mysql format
+                since = date.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss"),
+                m     = 3,
+                s     = 0,
+                limit
+            });
+
+            Console.WriteLine("Requesting maps ranked after " + date.ToString("yyyy-MM-dd HH:mm:ss"));
+
+            var resString = await url.GetStringAsync();
+            var json      = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(resString)!;
+
+            var listSub = json.Select(b =>
+                new BeatmapSet(int.Parse(b["beatmapset_id"]), b["title"], b["artist"], b["creator"])
+            ).ToList();
+
+            list.AddRange(listSub);
+            list = list.DistinctBy(x => x.Id).ToList();
+
+            var maxT = json.Max(x =>
+                DateTime.ParseExact(x["approved_date"], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)
+            );
+
+            var cacheFileName = $"{new DateTimeOffset(maxT).ToUnixTimeMilliseconds()}-ranked.json";
+            await File.WriteAllTextAsync(cacheFileName, JsonConvert.SerializeObject(listSub));
+
+            if (json.Count < limit) return list;
+        }
+    }
+
+    public static async Task<List<BeatmapSet>> GetUserBeatmapSets(string username)
+    {
+        var cacheName = DateTime.Now.ToString("[yyyy-MM-dd]") + $"[{username}].json";
+        if (File.Exists(cacheName))
+        {
+            return JsonConvert.DeserializeObject<List<BeatmapSet>>(await File.ReadAllTextAsync(cacheName))!;
+        }
+
+        var user = await OsuApi.Request(OsuApi.ApiRoot + $"/users/{username}/mania?key=username").GetJsonAsync();
+        var uri  = OsuApi.ApiRoot + $"/users/{user.id}/beatmapsets";
+
+        var type = new[] { "graveyard", "guest", "loved", "ranked" };
+
+        var res = new List<BeatmapSet>();
+
+        foreach (var t in type)
+        {
+            const int limit  = 50;
+            var       offset = 0;
+
+            while (true)
+            {
+                try
+                {
+                    var j = await OsuApi
+                        .Request($"{uri}/{t}?limit={limit}&offset={offset}")
+                        .GetJsonListAsync();
+
+                    res.AddRange(j.Select(x =>
+                        new BeatmapSet((int)x.id, (string)x.title, (string)x.artist, (string)x.creator))
+                    );
+                    if (j.Count < limit) break;
+                }
+                catch (Exception e)
+                {
+                    if (HandleException(e)) continue;
+                    break;
+                }
+
+                offset += limit;
+            }
+        }
+
+        await File.WriteAllTextAsync(cacheName, JsonConvert.SerializeObject(res));
+
+        return res;
+
+        bool HandleException(Exception e)
+        {
+            while (e is AggregateException ae)
+            {
+                e = ae.InnerExceptions.First();
+            }
+            if (e is not FlurlHttpException fe) return false;
+
+            Console.WriteLine(fe.Message);
+            return true;
+        }
     }
 
     #endregion
--- OsuBeatmapDownloader/Program.cs
+++ OsuBeatmapDownloader/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Concurrent;
-using System.Globalization;
-using Flurl;
 using Flurl.Http;
 using Newtonsoft.Json;
 using osu.Game.Beatmaps;
@@ -8,130 +6,37 @@ using Realms;
 
 namespace OsuBeatmapDownloader;
 
-internal record BeatmapSet(int Id, string Title, string Artist, string Creator);
-
 internal static class Program
 {
     private const string LazerPath = @"O:\GameStorage\osu!lazer";
     private const string Output = LazerPath;
 
-    private static HashSet<int> _downloaded = [];
+    private static readonly string[] RecommendMapperList =
+    [
+        // 7k 鱼丸推荐
+        "Evening", "Kim_GodSSI", "Jinjin", "_underjoy", "Wonki", "taba2", "Hehoooh", "paulkappa", "Remuring",
+        "paradoxus_",
+        "_Kobii", "Blocko", "ExNeko", "Alsty-", "Schopfer", "Tropicar", "_Stan", "erased self", "-NoName-",
+        "TakJun",
+        "Imperial Wolf", "AncuL",
+        //
+        "Muses", "quicalid4", "_Reimu", "m1n530k", "Laply", "Lung_P", "Emida", "yellEx", "Chyo_N", "Dety",
+        "emO_Oticon",
+        "Nananana", "My Angel Koishi", "arcwinolivirus", "MapleSyrup-", "Enie", "Akayro", "Arona", "kasumi99",
+        "Mage",
+        "JDS20", "GoosBaams", "AWMRone",
+        //
+        "sankansuki", "lenpai", "Flexo123", "qodtjr", "tangjinxi", "Kawawa", "17VA", "Reba", "Pengdoll", "LostCool",
+        "Critical_Star", "Rurikon_", "Wilben_Chan", "- Minato Aqua -", "Entozer", "Cuppp", "pwhk", "Nivrad00",
+        //
+        "tyrcs", "ruka", "Leeju",
+        // 6k 曲包的谱师
+        "QQwiwi2012", "Arkman", "[Crz]Derrick", "doubu", "_IceRain", "[Crz]Emperor", "Alipay", "HMillion", "Benson_",
+        "[Crz]sunnyxxy", "BKwind", "tyrcs",
+        // 7k 自己找的
+        "[Crz]Emperor-",
+    ];
 
-    private static async Task<List<BeatmapSet>> GetRankedBeatmapSets()
-    {
-        var list = new List<BeatmapSet>();
-        var cache =
-            from file in Directory.GetFiles(".", "*-ranked.json")
-            select JsonConvert.DeserializeObject<List<BeatmapSet>>(File.ReadAllTextAsync(file).Result);
-        foreach (var i in cache) list.AddRange(i);
-        list = list.DistinctBy(x => x.Id).ToList();
-
-        while (true)
-        {
-            var date = (
-                from file in Directory.GetFiles(".", "*-ranked.json")
-                select Path.GetFileNameWithoutExtension(file)
-                into dateInFile
-                select dateInFile.Split('-')[0]
-                into dateInFile
-                select DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(dateInFile))
-                into last
-                select last - TimeSpan.FromHours(2)
-            ).Aggregate(DateTimeOffset.MinValue, (current, last) => last > current ? last : current);
-
-            const int limit = 500;
-            var url = "https://osu.ppy.sh/api/get_beatmaps".SetQueryParams(new
-            {
-                k = OsuApi.Config["apiKey"],
-                // utc time in mysql format
-                since = date.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss"),
-                m     = 3,
-                s     = 0,
-                limit
-            });
-
-            Console.WriteLine("Requesting maps ranked after " + date.ToString("yyyy-MM-dd HH:mm:ss"));
-
-            var resString = await url.GetStringAsync();
-            var json      = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(resString);
-
-            var listSub = json.Select(b =>
-                new BeatmapSet(int.Parse(b["beatmapset_id"]), b["title"], b["artist"], b["creator"])
-            ).ToList();
-
-            list.AddRange(listSub);
-            list = list.DistinctBy(x => x.Id).ToList();
-
-            var maxT = json.Max(x =>
-                DateTime.ParseExact(x["approved_date"], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)
-            );
-
-            var cacheFileName = $"{new DateTimeOffset(maxT).ToUnixTimeMilliseconds()}-ranked.json";
-            await File.WriteAllTextAsync(cacheFileName, JsonConvert.SerializeObject(listSub));
-
-            if (json.Count < limit) return list;
-        }
-    }
-
-    private static bool HandleException(Exception e)
-    {
-        while (e is AggregateException ae)
-        {
-            e = ae.InnerExceptions.First();
-        }
-        if (e is not FlurlHttpException fe) return false;
-
-        Console.WriteLine(fe.Message);
-        return true;
-    }
-
-    private static async Task<List<BeatmapSet>> GetUserBeatmapSets(string username)
-    {
-        var cacheName = DateTime.Now.ToString("[yyyy-MM-dd]") + $"[{username}].json";
-        if (File.Exists(cacheName))
-        {
-            return JsonConvert.DeserializeObject<List<BeatmapSet>>(await File.ReadAllTextAsync(cacheName));
-        }
-
-        var user = await OsuApi.Request(OsuApi.ApiRoot + $"/users/{username}/mania?key=username").GetJsonAsync();
-        var uri  = OsuApi.ApiRoot + $"/users/{user.id}/beatmapsets";
-
-        var type = new[] { "graveyard", "guest", "loved", "ranked" };
-
-        var res = new List<BeatmapSet>();
-
-        foreach (var t in type)
-        {
-            const int limit  = 50;
-            var       offset = 0;
-
-            while (true)
-            {
-                try
-                {
-                    var j = await OsuApi
-                        .Request($"{uri}/{t}?limit={limit}&offset={offset}")
-                        .GetJsonListAsync();
-
-                    res.AddRange(j.Select(x =>
-                        new BeatmapSet((int)x.id, (string)x.title, (string)x.artist, (string)x.creator))
-                    );
-                    if (j.Count < limit) break;
-                }
-                catch (Exception e)
-                {
-                    if (HandleException(e)) continue;
-                    break;
-                }
-
-                offset += limit;
-            }
-        }
-
-        await File.WriteAllTextAsync(cacheName, JsonConvert.SerializeObject(res));
-
-        return res;
-    }
 
     private static async Task<List<BeatmapSet>> GetRecommendMapList()
     {
@@ -140,42 +45,18 @@ internal static class Program
         if (File.Exists(cacheName))
         {
             // deserialize
-            return JsonConvert.DeserializeObject<List<BeatmapSet>>(await File.ReadAllTextAsync(cacheName));
+            return JsonConvert.DeserializeObject<List<BeatmapSet>>(await File.ReadAllTextAsync(cacheName))!;
         }
-
-        var userList = new[]
-        {
-            // 7k
-            "Evening", "Kim_GodSSI", "Jinjin", "_underjoy", "Wonki", "taba2", "Hehoooh", "paulkappa", "Remuring",
-            "paradoxus_",
-            "_Kobii", "Blocko", "ExNeko", "Alsty-", "Schopfer", "Tropicar", "_Stan", "erased self", "-NoName-",
-            "TakJun",
-            "Imperial Wolf", "AncuL",
-            //
-            "Muses", "quicalid4", "_Reimu", "m1n530k", "Laply", "Lung_P", "Emida", "yellEx", "Chyo_N", "Dety",
-            "emO_Oticon",
-            "Nananana", "My Angel Koishi", "arcwinolivirus", "MapleSyrup-", "Enie", "Akayro", "Arona", "kasumi99",
-            "Mage",
-            "JDS20", "GoosBaams", "AWMRone",
-            //
-            "sankansuki", "lenpai", "Flexo123", "qodtjr", "tangjinxi", "Kawawa", "17VA", "Reba", "Pengdoll", "LostCool",
-            "Critical_Star", "Rurikon_", "Wilben_Chan", "- Minato Aqua -", "Entozer", "Cuppp", "pwhk", "Nivrad00",
-            //
-            "tyrcs", "ruka", "Leeju",
-            // 6k
-            "QQwiwi2012", "Arkman", "[Crz]Derrick", "doubu","_IceRain", "[Crz]Emperor", "Alipay", "HMillion", "Benson_",
-            "[Crz]sunnyxxy"
-        };
 
         var mapList = new List<BeatmapSet>();
 
-        Parallel.ForEach(userList.Distinct(), new ParallelOptions { MaxDegreeOfParallelism = 8 }, user =>
+        Parallel.ForEach(RecommendMapperList.Distinct(), new ParallelOptions { MaxDegreeOfParallelism = 8 }, user =>
         {
             RETRY:
             try
             {
                 Console.WriteLine("Requesting " + user);
-                var res = GetUserBeatmapSets(user).Result;
+                var res = OsuApi.GetUserBeatmapSets(user).Result;
                 lock (mapList)
                 {
                     mapList.AddRange(res);
@@ -208,22 +89,24 @@ internal static class Program
         var fpNew = Path.Join(LazerPath, "client.realm.new");
         File.Copy(fp, fpNew, true);
 
+        HashSet<int> downloaded;
         using (var realm = Realm.GetInstance(new RealmConfiguration(fpNew) { SchemaVersion = 4700 }))
         {
             var info = realm.All<BeatmapSetInfo>().ToList().Select(x => x.OnlineID);
-            _downloaded = info.ToHashSet();
+            downloaded = info.ToHashSet();
         }
         File.Delete(fpNew);
         File.Delete(fpNew + ".lock");
 
         return mapList
             .DistinctBy(x => x.Id)
-            .Where(x => !_downloaded.Contains(x.Id))
+            .Where(x => !downloaded.Contains(x.Id))
             .ToList();
     }
 
     private static int _cnt;
     private static readonly object CntLk = new();
+
     private static void DownloadTask(ConcurrentQueue<(BeatmapSet Map, int Count)> queue, int maxRetry)
     {
         while (queue.TryDequeue(out var map))
@@ -262,7 +145,7 @@ internal static class Program
         }
 
         var recommendMapList = await GetRecommendMapList();
-        var rankedMapList    = await GetRankedBeatmapSets();
+        var rankedMapList    = await OsuApi.GetRankedBeatmapSets();
 
         var mapList = rankedMapList.Concat(recommendMapList).DistinctBy(x => x.Id).ToList();
         mapList = RemoveDownloadedBeatmaps(mapList);
